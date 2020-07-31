a9460588590e6852048eba8ba1fb16964eaba233
St.Anton <striuchkov.anton@gmail.com>
2020-07-31 17:33:40 +0300

    Добавляет библиотеку Redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions.js";
+import {reducer} from "./reducer.js";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 const Settings = {
   ERRORS_COUNT: 3,
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App errorsCount={Settings.ERRORS_COUNT} questions={questions} />,
+    <Provider store={store}>
+      <App errorsCount={Settings.ERRORS_COUNT} questions={questions} />,
+    </Provider>,
     document.querySelector(`#root`)
 );
