997c19677849829d4ef99cf96a657f421c139f86
St.Anton <striuchkov.anton@gmail.com>
2020-08-03 12:42:12 +0300

    Добавит проверку ответов, подключит моки для store, подключит компонент game-screen к store, добавит компонент Mistakes

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
-
+import {connect} from "react-redux";
+import Mistakes from "../mistakes/mistakes.jsx";
 
 const GameScreen = (props) => {
-  const {
-    type,
-    children,
-  } = props;
+  const {type, children, mistakes} = props;
 
   return (
     <section className={`game game--${type}`}>
       <header className="game__header">
         <a className="game__back" href="#">
           <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+          <img
+            className="game__logo"
+            src="img/melody-logo-ginger.png"
+            alt="Угадай мелодию"
+          />
         </a>
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="timer"
+          viewBox="0 0 780 780"
+        >
+          <circle
+            className="timer__line"
+            cx="390"
+            cy="390"
+            r="370"
+            style={{
+              filter: `url(#blur)`,
+              transform: `rotate(-90deg) scaleY(-1)`,
+              transformOrigin: `center`,
+            }}
+          />
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        <Mistakes count={mistakes} />
       </header>
 
       {children}
@@ -34,14 +45,18 @@ const GameScreen = (props) => {
   );
 };
 
-
 GameScreen.propTypes = {
   type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
+    PropTypes.node,
   ]).isRequired,
+  mistakes: PropTypes.number.isRequired,
 };
 
+const mapStateToProps = (state) => ({
+  mistakes: state.mistakes,
+});
 
-export default GameScreen;
+export {GameScreen};
+export default connect(mapStateToProps)(GameScreen);
