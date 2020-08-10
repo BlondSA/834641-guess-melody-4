50b093a67d1b63dd97b0469daf525d2c838a678f
St.Anton <striuchkov.anton@gmail.com>
2020-08-10 20:09:44 +0300

    Вынесет состояние из компонента Жанра в HOC

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -3,17 +3,14 @@ import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 
 class GenreQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      answers: [false, false, false, false],
-    };
-  }
-
   render() {
-    const {onAnswer, question, renderPlayer} = this.props;
-    const {answers: userAnswers} = this.state;
+    const {
+      onAnswer,
+      onChange,
+      question,
+      renderPlayer,
+      userAnswers,
+    } = this.props;
     const {answers, genre} = question;
 
     return (
@@ -23,7 +20,7 @@ class GenreQuestionScreen extends PureComponent {
           className="game__tracks"
           onSubmit={(evt) => {
             evt.preventDefault();
-            onAnswer(question, this.state.answers);
+            onAnswer();
           }}
         >
           {answers.map((answer, i) => (
@@ -66,6 +63,8 @@ class GenreQuestionScreen extends PureComponent {
 }
 
 GenreQuestionScreen.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  userAnswers: PropTypes.arrayOf(PropTypes.bool).isRequired,
   onAnswer: PropTypes.func.isRequired,
   question: PropTypes.shape({
     answers: PropTypes.arrayOf(
